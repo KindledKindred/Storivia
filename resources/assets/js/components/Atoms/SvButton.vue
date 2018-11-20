8620dfa723e41d59086db8af017a21681207f200
opion <kindledkindred@gmail.com>
2018-11-20 16:28:36 +0900

    atoms
    create atoms w/o contents

---
+++ resources/assets/js/components/Atoms/SvButton.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+    button(:class="classes" :disabled="disabled" type="button" @click="handleClick")
+        slot
+</template>
+
+<script>
+export default {
+    name: 'SvButton',
+
+    props: {
+        disabled: {
+            tyle: Boolean,
+            default: false
+        },
+        type: {
+            type: String,
+            default: 'button'
+        }
+    },
+
+    computed: {
+        //'type'に応じて動的にクラスを生成する
+        classes () {
+            const cls = this.type === 'text' ? ('-' + this.type) : ''
+            return ['sv-button${cls}']
+        }
+    },
+
+    methods: {
+        //'click'イベントを発行
+        handleClick (ev) {
+            this.$emit('click', ev)
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.sv-button
+    padding .6em 1.3em
+
+.sv-button-text
+    border none 
+    padding-right 0
+    padding-left 0
+</style>
+
+
---
+++ resources/assets/js/components/Molecules/SvLoginForm.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+    form(novalidate)
+        .form-item
+            lavel(for="email") メールアドレス
+            input(
+                id="email"
+                v-model="email"
+                type="text"
+                autocomplete="off"
+                placeholder="example@domain.com"
+                @focus="resetError")
+            ul.validation-errors
+                li(v-if="!validation.email.format") メールアドレスの形式が不正です
+                li(v-if="!validation.email.required") メールアドレスが入力されていません
+        .form-item
+            label(for="password") パスワード
+            input(
+                id="password"
+                v-model="password"
+                type="password"
+                autocomplete="off"
+                placeholder="password"
+                @focus="resetError"
+            )
+            ul.validation-errors
+                li(v-if="validation.password.required") パスワードが未入力です
+        .form-actions
+            SvButton(:disabled="disableLoginAction" @click="handleClick") ログイン
+            p.login-progress(v-if="progress") ログイン中．．．
+            p.login-error(v-if="error") @{{ error }}
+</template>
